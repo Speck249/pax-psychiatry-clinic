fd3b63fe750054705efa913aa5d341f274d37b5f
Rediet <redietkfle@gmail.com>
2024-06-12 14:35:08 +0300

    Update Doctors page name and component layout

---
+++ src/pages/doctors.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Doctors from '../components/Doctors/Doctors';
+import Footer from '../components/Footer/Footer';
+
+const DoctorsPage = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+return (
+  <>
+    <Navbar toggle={toggle}/>
+    <Sidebar isOpen={isOpen} toggle={toggle}/>
+    <Doctors />
+    <Footer />
+    </>
+  )
+}
+
+export default DoctorsPage;
