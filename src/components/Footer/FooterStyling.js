a3a19d1598d2822b8cfeb890acf64e7fbd6e3678
Rediet <redietkfle@gmail.com>
2024-06-12 18:10:43 +0300

    Update Footer styling and fix redundancies

--- src/components/Footer/FooterStyling.js
+++ src/components/Footer/FooterStyling.js
@@ -1,48 +1,53 @@
 import styled from 'styled-components';
 import { Link as LinkRouter } from 'react-router-dom';
 
+// Social Media Icons
+// Web Breakpoints
 export const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-height: 100%;
+  height: 55vh;
   background-color: #000;
 `;
 
 export const FooterWrap = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-around;
   align-items: center;
-  padding-top: 50px;
+  max-width: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 export const Newsletter = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  padding: 1rem;
 `;
 
 export const NewsletterTitle = styled.h2`
   color: #fff;
-  font-size: 19px;
+  font-size: 18px;
   text-align: center;
-  margin-bottom: 1rem;
+  line-height: 1.8;
 `;
 
 export const NewsletterBody = styled.h3`
-  color: #e6e6e6;
   font-size: 14px;
-  font-weight: 400;
+  font-weight: 300;
   text-align: center;
-  line-height: 1.0;
+  margin-bottom: 1.5rem;
+  color: #e6e6e6;
 `;
 
 export const Form = styled.form`
   display: flex;
-  align-items: center;
-  margin-top: 1rem;
-  margin-bottom: 2rem;
-
-  @media only screen and (max-width: 820px) {
-    flex-direction: column;
-    flex-flow: wrap;
-  }
+  flex-direction: row;
+  gap: 10px;
 `;
 
 export const FormLabel = styled.label`
@@ -50,94 +55,69 @@ export const FormLabel = styled.label`
 `;
 
 export const FormInput = styled.input`
-   color: #000;
-   text-align: left;
    font-size: 14px;
    width: 100%;
-   padding-left: 20px;
-   padding-right: 30px;
-   padding-bottom: 10px;
-   padding-top: 9px;
-   margin-right: 2rem;
-   border: 1px solid #000;
+   padding: 0.75rem 1.2rem;
+   border: none;
    border-radius: 2px;
-   outline: none;
 
    &::placeholder {
-    color: #000;
-    font-size: 0.9rem;
-   }
-
-   @media only screen and (max-width: 820px) {
-    padding: 9px 25px;
-    margin-bottom: 0.8;
-    font-size: 1.2rem;
+    color: gray;
+    font-size: 12px;
    }
 `;
 
 export const FormButton = styled.button`
-   font-size: 1rem;
+   font-size: 14px;
    color: #fff;
-   outline: none;
-   padding: 0.6rem 1.2rem;
-   margin-left: 15px;
-   border-radius: 3px;
+   padding: 0.75rem 1rem;
    border: none;
+   border-radius: 2px;
    background-color: #09798C;
    cursor: pointer;
 `;
 
-export const FooterLinkContainer = styled.div`
+export const SocialMediaLinks = styled.div`
    display: flex;
-
-  @media screen and (max-width: 820px) {
-    padding-top: 12px;
-  }
+   flex-direction: row;
+   justify-content: space-evenly;
+   align-items: center;
+   padding: 0.5rem;
+   margin-top: 1.5rem;
+   cursor: pointer;
 `;
 
 export const FooterLinkWrapper = styled.div`
   display: flex;
-
-  @media screen and (max-width: 820px) {
-    flex-direction: column;
-  }
+  flex-direction: row;
+  justify-content: center;
+  align-self: center;
+  padding: 1rem;
+  width: 40%;
+  gap: 70px;
 `;
 
 export const FooterLinks = styled.div`
-  color: #fff;
-  text-align: left;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  margin: 10px;
-  width: 155px;
-  box-sizing: border-box;
-
-  @media screen and (max-width: 420px) {
-    margin: 0;
-    padding: 10px;
-    width: 100%;
-  }
 `;
 
 export const FooterLinkName = styled.h1`
-  font-size: 14px;
+  font-size: 15px;
   font-weight: bold;
-  margin-bottom: 12px;
+  color: #fff;
 `;
 
 export const FooterLink = styled(LinkRouter)`
-  color: #fff;
   font-size: 12px;
   text-decoration: none;
-  margin-bottom: 0.5rem;
-  margin-right: 1rem;
+  line-height: 2.5;
+  color: #fff;
 `;
 
 export const FooterCopyright = styled.small`
-  color: #fff;
+  padding: 0.5rem;
   font-size: 10px;
   text-align: center;
-  margin-top: 13px;
-  margin-bottom: 13px;
+  color: #fff;
 `;
