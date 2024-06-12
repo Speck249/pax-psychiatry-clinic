5240bc9b03cb7dfc97557cd6d8e5bf99529166f8
Rediet <redietkfle@gmail.com>
2024-06-12 14:33:36 +0300

    Update About page component layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Sidebar from '../components/Sidebar/Sidebar';
+import About from '../components/About/About';
+import InfoSection from '../components/About/InfoSection';
+import { featureSection } from '../components/About/DataFile';
 import Footer from '../components/Footer/Footer';
-import AboutPage from '../components/About/About';
 
-const About = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+const AboutPage = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
 
-return (
-  <>
-    <Navbar toggle={toggle}/>
-    <Sidebar isOpen={isOpen} toggle={toggle}/>
-    <AboutPage />
-    <Footer />
+  return (
+    <>
+      <Navbar toggle={toggle}/>
+      <Sidebar isOpen={isOpen} toggle={toggle}/>
+      <About />
+      <InfoSection {...featureSection}/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default About;
+export default AboutPage;
