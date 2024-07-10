7321d3e5282dd15dbe509c2f1749f89850656b30
Rediet <redietkfle@gmail.com>
2024-07-10 12:44:09 +0300

    Adjust component for responsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,118 +1,82 @@
 import React, { useState } from 'react';
-import { Alert } from 'react-bootstrap';
+/* import { Alert } from 'react-bootstrap';
 import { collection, addDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+import { db } from '../../firebase'; */
 import { FaFacebook, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
 import {
-    FooterContainer,
-    FooterWrap,
-    Newsletter,
-    NewsletterTitle,
-    NewsletterBody,
-    Form, 
-    FormLabel,
-    FormInput,
-    FormButton,
-    SocialMediaLinks,
-    FooterLinkWrapper,
-    FooterLinks,
-    FooterLinkName,
-    FooterLink,
-    FooterCopyright
+  FooterContainer,
+  FooterWrapper,
+  NewsletterContainer,
+  NewsletterTitle,
+  NewsletterDescription,
+  SubscriptionContainer,
+  SubscriptionLabel,
+  SubscriptionBox,
+  SubscriptionButton,
+  SocialMediaContainer,
+  FooterLinkContainer,
+  FooterLinkHeader,
+  FooterLinkList,
+  FooterLink,
+  FooterCopyright
 } from './FooterStyling';
 
 
 const Footer = () => {
-    const [email, setEmail] = useState('');
-    const [subscriptionStatus, setSubscriptionStatus] = useState('');
-    const handleSubmit = async (e) => {
-      try {
-        const newsletterData = {
-            email
-        };
-        const docRef = await addDoc(collection(db, 'newsletters'), newsletterData);
-        setEmail('');    
-        setSubscriptionStatus(true);
-        setTimeout(() => {
-          setSubscriptionStatus(null);
-        }, 5000);
-      } catch (error) {
-        setSubscriptionStatus(false);    
-        setTimeout(() => {
-          setSubscriptionStatus(null);
-        }, 5000);
-      };
-    };
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+      <NewsletterContainer>
+        <NewsletterTitle>Subscribe to Our Weekly Newsletter</NewsletterTitle>
+        <NewsletterDescription>Get Exclusive Health Content Curated by Industry Experts.</NewsletterDescription>
+        <SubscriptionContainer>
+          <SubscriptionLabel>Email</SubscriptionLabel>
+          <SubscriptionBox id='email' type='email' placeholder='asterawoke@gmail.com' autoComplete='' ></SubscriptionBox>
+          <SubscriptionButton>Subscribe</SubscriptionButton>
+        </SubscriptionContainer>
+        <SocialMediaContainer>
+          <FaFacebook to='https://www.facebook.com' target='_blank' size={30} color='#fff'></FaFacebook>
+          <FaTwitter to='https://www.twitter.com' target='_blank' size={30} color='#fff'></FaTwitter>
+          <FaLinkedin to='https://www.linkedin.com' target='_blank' size={30} color='#fff'></FaLinkedin>
+          <FaInstagram to='https://www.instagram.com' target='_blank' size={30} color='#fff'></FaInstagram>
+        </SocialMediaContainer>
+      </NewsletterContainer>
 
-    return (
-      <FooterContainer>
-        <FooterWrap>
-          <Newsletter>
-            <NewsletterTitle>
-              Subscribe to Our Weekly Newsletter.
-            </NewsletterTitle>
-            <NewsletterBody>
-              Get Exclusive Health Content Curated by Industry Experts.
-            </NewsletterBody>
-            {subscriptionStatus === true && <Alert variant="success">Welcome to the PAX Community!</Alert>}
-            {subscriptionStatus === false && <Alert variant="danger">Subscription Failed. Please try again.</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <FormLabel>Email Address</FormLabel>
-              <FormInput
-                type='email'
-                id='email'
-                value={email}
-                placeholder='Insert Email' 
-                onChange={(e) => setEmail(e.target.value)} required
-              />
-              <FormButton variant='primary' type='submit'>Subscribe</FormButton>
-            </Form>
+      <FooterLinkContainer>
+        <FooterLinkList>
+          <FooterLinkHeader>HOSPITAL</FooterLinkHeader>
+          <FooterLink>About Us</FooterLink>
+          <FooterLink>Our Medical Team</FooterLink>
+          <FooterLink>Our Facilities</FooterLink>
+          <FooterLink>Testimonials</FooterLink>
+          <FooterLink>Careers</FooterLink>
+          <FooterLink>FAQs</FooterLink>
+        </FooterLinkList>
 
-            <SocialMediaLinks>
-              <FaFacebook to='https://www.facebook.com/' target= '_blank' size={30} color='#fff' />
-              <FaLinkedin to='https://www.linkedin.com/' target='_blank' size={30} color='#fff' />
-              <FaTwitter to='https://www.twitter.com/' target='_blank' size={30} color='#fff' />
-              <FaInstagram to='https://www.instagram.com/' target='_blank' size={30} color='#fff' />
-              </SocialMediaLinks>
-          </Newsletter>
+        <FooterLinkList>
+          <FooterLinkHeader>SERVICES</FooterLinkHeader>
+          <FooterLink>Adolescent Therapy</FooterLink>
+          <FooterLink>Family Therapy</FooterLink>
+          <FooterLink>Couples Therapy</FooterLink>
+          <FooterLink>Marriage Counselling</FooterLink>
+          <FooterLink>Corporate Counselling</FooterLink>
+          <FooterLink>Psychiatry</FooterLink>
+        </FooterLinkList>
 
-          <FooterLinkWrapper>
-            <FooterLinks>
-              <FooterLinkName>HOSPITAL</FooterLinkName>
-              <FooterLink to='/about'>About Us</FooterLink>
-              <FooterLink to='/doctors'>Our Medical Team</FooterLink>
-              <FooterLink to='#'>Our Facilities</FooterLink>
-              <FooterLink to='#'>Patient's Testimonials</FooterLink>
-              <FooterLink to='#'>Careers</FooterLink>
-              <FooterLink to='#'>FAQs</FooterLink>
-            </FooterLinks>
-                    
-            <FooterLinks>
-              <FooterLinkName>SERVICES</FooterLinkName>
-              <FooterLink to='/services'>Adolescent Therapy</FooterLink>
-              <FooterLink to='/services'>Family Therapy</FooterLink>
-              <FooterLink to='/services'>Couples Therapy</FooterLink>
-              <FooterLink to='/services'>Marriage Counselling</FooterLink>
-              <FooterLink to='/services'>Psychiatry</FooterLink>
-              <FooterLink to='/services'>Corporate Counselling</FooterLink>
-            </FooterLinks>
-
-            <FooterLinks>
-              <FooterLinkName>RESOURCES</FooterLinkName>
-              <FooterLink to='https://www.medium.com/' target='_blank'>Blog</FooterLink>
-              <FooterLink to='#'>Medical Journal</FooterLink>
-              <FooterLink to='#'>Price Transparency</FooterLink>
-              <FooterLink to='#'>Patient & Visitor Policy</FooterLink>
-              <FooterLink to='#'>Terms & Conditions</FooterLink>
-            </FooterLinks>
-          </FooterLinkWrapper>
-        </FooterWrap>
-
-        <FooterCopyright to='https://github.com/Speck249/'>
-          PAX &copy; {new Date().getFullYear()} All Rights Reserved
-        </FooterCopyright>
-      </FooterContainer>
-    )
+        <FooterLinkList>
+          <FooterLinkHeader>RESOURCES</FooterLinkHeader>
+          <FooterLink>Research Journal</FooterLink>
+          <FooterLink>Innovative Endeavors</FooterLink>
+          <FooterLink>In the News</FooterLink>
+          <FooterLink>Price Transparency</FooterLink>
+          <FooterLink>Patient & Visitor Policy</FooterLink>
+          <FooterLink>Terms & Conditions</FooterLink>
+        </FooterLinkList>
+      </FooterLinkContainer>
+    </FooterWrapper>
+      <FooterCopyright>Copyright &copy; {new Date().getFullYear()} Pax Therapy Clinic. All rights reserved.</FooterCopyright>
+    </FooterContainer>
+  )
 };
 
-export default Footer;
+export default Footer; 
