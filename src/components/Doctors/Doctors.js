8c9f1b6b508b5e926c874e2bc3ca0097522c7e44
rediet <redietkfle@gmail.com>
2024-08-04 18:28:55 +0300

    Update Doctors Page Design

--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import {
-  TeamSection,
-  TeamIntroduction,
+  MedicalTeamSection,
+  SectionBrief,
   SectionTitle,
   SectionDescription,
-  ParagraphImg,
-  TeamContainer,
-  MedicalTeamOne,
-  MoreDescription,
-  MedicalTeamTwo,
-  MedicalTeamThree,
-  MedicalTeamFour,
-  MedicalTeamFive,
-  MedicalTeamSix,
+  ImageContainer,
+  Image,
+  MedicalTeamContainer,
   ContentContainer,
+  FirstDoctor,
+  SecondDoctor,
+  ThirdDoctor,
+  FourthDoctor,
+  FifthDoctor,
+  SixthDoctor,
   DoctorName,
-  DoctorDescription,
-} from './DoctorsStyling';
-import wreath from '../../images/ParagraphImg.png';
+  MedicalTitle,
+  AdditionalDescription
+} from "./DoctorsStyling";
+import greyLeaf from "../../images/Grey-Leaf.png";
 
 
 const Doctors = () => {
   return (
-    <TeamSection>
-      <TeamIntroduction>
+    <MedicalTeamSection>
+      <SectionBrief>
         <SectionTitle>Building Stronger Minds, One Patient at a Time</SectionTitle>
         <SectionDescription>
           We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
@@ -31,95 +32,103 @@ const Doctors = () => {
           With a deep understanding of the complexities of the human mind, our clinicians employ the latest evidence-based treatments to help our patients navigate their mental
           health journeys with compassion and care.  
         </SectionDescription>
-        <ParagraphImg src={wreath} width={500} height={500}></ParagraphImg>
-      </TeamIntroduction>
+        <ImageContainer>
+          <Image src={greyLeaf} width={500} height={500} alt="Decorative Leaf Image"></Image>
+        </ImageContainer>
+      </SectionBrief>
 
-      <TeamContainer>
-        <MedicalTeamOne>
+      <MedicalTeamContainer>
+        <FirstDoctor>
           <ContentContainer>
             <DoctorName>Dr. Aisha Nkosi</DoctorName>
-            <DoctorDescription>Clinical Psychologist</DoctorDescription>
+            <MedicalTitle>Clinical Psychologist | Clinical Director</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
-            <strong>Dr. Kwame Osei</strong> <br />
-            <strong>Adolescent Therapist</strong> <br /> <br />
+
+          <AdditionalDescription>
+            <strong>Dr. Aisha Nkosi</strong> <br />
+            <strong>Clinical Psychologist | Clinical Director</strong> <br /> <br />
               We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world.
-          </MoreDescription>
-        </MedicalTeamOne>
+          </AdditionalDescription>
+        </FirstDoctor>
 
-        <MedicalTeamTwo>
+        <SecondDoctor>
           <ContentContainer>
             <DoctorName>Dr. Kwame Osei</DoctorName>
-            <DoctorDescription>Adolescent Therapist</DoctorDescription>
+            <MedicalTitle>Adolescent Therapist | Clinical Lead</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
+
+          <AdditionalDescription>
             <strong>Dr. Kwame Osei</strong> <br />
-            <strong>Adolescent Therapist</strong> <br /> <br />
+            <strong>Adolescent Therapist | Clinical Lead</strong> <br /> <br />
               We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world.
-          </MoreDescription>
-        </MedicalTeamTwo>
+          </AdditionalDescription>
+        </SecondDoctor>
 
-        <MedicalTeamThree>
+        <ThirdDoctor>
           <ContentContainer>
             <DoctorName>Dr. Nia Jelani</DoctorName>
-            <DoctorDescription>Family Therapist</DoctorDescription>
+            <MedicalTitle>Couples & Marriage Counsellor</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
-            <strong>Dr. Kwame Osei</strong> <br />
-            <strong>Adolescent Therapist</strong> <br /> <br />
+
+          <AdditionalDescription>
+            <strong>Dr. Nia Jelani</strong> <br />
+            <strong>Couples & Marriage Counsellor</strong> <br /> <br />
               We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world.
-          </MoreDescription>
-        </MedicalTeamThree>
+          </AdditionalDescription>
+        </ThirdDoctor>
         
-        <MedicalTeamFour>
+        <FourthDoctor>
           <ContentContainer>
             <DoctorName>Dr. Zara Abiola</DoctorName>
-            <DoctorDescription>Couples & Marriage Counsellor</DoctorDescription>
+            <MedicalTitle>Couples & Marriage Counsellor</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
-            <strong>Dr. Kwame Osei</strong> <br />
-            <strong>Adolescent Therapist</strong> <br /> <br />
+
+          <AdditionalDescription>
+            <strong>Dr. Zara Abiola</strong> <br />
+            <strong>Couples & Marriage Counsellor</strong> <br /> <br />
               We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields.
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world. 
-          </MoreDescription> 
-        </MedicalTeamFour>
+          </AdditionalDescription> 
+        </FourthDoctor>
 
-        <MedicalTeamFive>
+        <FifthDoctor>
           <ContentContainer>
             <DoctorName>Dr. Tariq Adewale</DoctorName>
-            <DoctorDescription>Clinical Psychiatrist</DoctorDescription>
+            <MedicalTitle>Clinical Psychiatrist</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
-            <strong>Dr. Kwame Osei</strong> <br />
-            <strong>Adolescent Therapist</strong> <br /> <br />
+
+          <AdditionalDescription>
+            <strong>Dr. Tariq Adewale</strong> <br />
+            <strong>Clinical Psychiatrist</strong> <br /> <br />
               We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields.
               Our psychiatrists have trained at some of the most prestigious medical institutions across the world. 
-          </MoreDescription>
-        </MedicalTeamFive>
+          </AdditionalDescription>
+        </FifthDoctor>
 
-        <MedicalTeamSix>
+        <SixthDoctor>
           <ContentContainer>
             <DoctorName>Dr. Jabari Sibanda</DoctorName>
-            <DoctorDescription>Marriage & Family Counsellor</DoctorDescription>
+            <MedicalTitle>Marriage & Family Counsellor</MedicalTitle>
           </ContentContainer>
-          <MoreDescription>
-             <strong>Dr. Kwame Osei</strong> <br />
-             <strong>Adolescent Therapist</strong> <br /> <br />
+          
+          <AdditionalDescription>
+             <strong>Dr. Jabari Sibanda</strong> <br />
+             <strong>Marriage & Family Counsellor</strong> <br /> <br />
                We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
                Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields.
                Our psychiatrists have trained at some of the most prestigious medical institutions across the world. 
-          </MoreDescription>
-        </MedicalTeamSix>
-      </TeamContainer>
-    </TeamSection>
+          </AdditionalDescription>
+        </SixthDoctor>
+      </MedicalTeamContainer>
+    </MedicalTeamSection>
   )
 };
 
