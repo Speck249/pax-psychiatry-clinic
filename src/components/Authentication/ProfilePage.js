5df45f9ca758d797cb9695126222665a55fedd93
rediet <redietkfle@gmail.com>
2024-08-18 17:57:37 +0300

    Profile Page Layout

---
+++ src/components/Authentication/ProfilePage.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useUserAuth } from '../../context/userAuthContext';
+import Dashboard from '../Dashboard/Dashboard';
+import { PageContainer, Button } from './ProfileStyling';
+
+const ProfilePage = () => {
+  const navigate = useNavigate();
+  const { logOut, user } = useUserAuth();
+  const [error, setError] = useState(null);
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+      setError('Error Logging Out.');
+      console.log(error);
+    }
+  };
+
+  return (
+    <PageContainer>
+      <Dashboard />
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Button to="/" onClick={handleLogout}>Log Out</Button>
+    </PageContainer>
+  );
+};
+
+export default ProfilePage;
