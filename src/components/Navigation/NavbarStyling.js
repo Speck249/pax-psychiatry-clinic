34e3c4ed19621f578e1ec1bec3ecc19f2306782e
rediet <redietkfle@gmail.com>
2025-03-05 16:00:43 +0300

    Update component responsiveness

--- src/components/Navigation/NavbarStyling.js
+++ src/components/Navigation/NavbarStyling.js
@@ -1,34 +1,29 @@
-import { styled, css } from "styled-components";
+import { styled } from "styled-components";
 import { Link as LinkRouter } from "react-router-dom";
 import { Link as LinkScroll } from "react-scroll";
 
 
-export const DisplayLayout = css`
+export const NavbarHeader = styled.header `
   display: flex;
   align-items: center;
-`;
-
-export const NavbarHeader = styled.header `
-  ${ DisplayLayout };
   justify-content: space-between;
   position: sticky;
   top: 0;
   width: 100%;
-  padding: 0.25rem 3rem;
-  margin: 0;
+  height: 100%;
+  padding: 0 3rem;
   z-index: 10;
   overflow: hidden;
   background-color: #fff;
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1024px) {
     background-color: #09798C;
   }
 
-  @media screen and (min-width: 800px) and (max-width: 1130px) {
+  @media screen and (min-width: 1100px) and (max-width: 1200px) {
     display: flex;
     margin-right: auto;
     margin-left: 0;
-    width: 100%;
   }
 `;
 
@@ -37,12 +32,13 @@ export const LogoContainer = styled(LinkRouter)`
 `;
 
 export const NavbarLogo = styled.img`
-  ${ DisplayLayout };
-  margin: 0.2rem 0.5rem;
+  display: flex;
+  align-items: center;
+  margin: 0.2rem 0;
   max-width: 85px;
   max-height: 80px;
 
-  @media screen and (max-width: 1130px) {
+  @media screen and (max-width: 1150px) {
     display: none;
   }
 `;
@@ -50,11 +46,12 @@ export const NavbarLogo = styled.img`
 export const SidebarLogo = styled.img `
   display: none;
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1024px) {
     display: block;
-    max-width: 93px;
+    max-width: 90px;
     max-height: 80px;
-    margin: 0.25rem 0;
+    margin: 0.2rem 0;
+    padding: 0;
 
     &:hover {
       cursor: pointer;
@@ -65,7 +62,7 @@ export const SidebarLogo = styled.img `
 export const MobileIcon = styled.div`
   display: none;
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1024px) {
     display: block;
     margin-left: auto;
     color: #fff;
@@ -77,31 +74,28 @@ export const MobileIcon = styled.div`
 `;
 
 export const NavbarWrapper = styled.nav`
-  ${ DisplayLayout };
-  width: 800px;
-  height: 100%;
+  display: flex;
+  align-items: center;
+  width: 750px;
   overflow: hidden;
   padding: 0;
   margin: 0;
 
-  @media screen and (max-width: 833px) {
+  @media screen and (max-width: 1024px) {
     display: none;
-  }
-
-  @media screen and (min-width: 834px) and (max-width: 1130px) {
-    margin-right: auto;
   }
 `;
 
 export const NavbarMenu = styled.ul`
-  ${ DisplayLayout };
+  display: flex;
+  align-items: center;
   justify-content: space-around;
   width: 100%;
   height: inherit;
   padding: 0.5rem 0;
   margin: 0;
 
-  @media screen and (min-width: 834px) and (max-width: 1130px) {
+  @media screen and (min-width: 1024px) and (max-width: 1200px) {
     width: 100%;
     height: inherit;
   }
@@ -138,18 +132,19 @@ export const RouterLink = styled(LinkRouter)`
   text-decoration: none;
   color: #09798C;
   list-style: none;
-
-  &:hover {
-    cursor: pointer;
-  }
 `;
 
 export const ButtonContainer = styled.div`
-  ${ DisplayLayout };
+  display: flex;
+  align-items: center;
   justify-content: center;
-  padding: 0.5rem 0;
+  padding: 1rem 0; 
   
-  @media screen and (max-width: 834px) {
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 1024px) {
     display: none;
   }
 `;
@@ -157,13 +152,9 @@ export const ButtonContainer = styled.div`
 export const SignIn = styled(LinkRouter)`
   color: #fff;
   text-decoration: none;
-  padding: 0.75rem 1.8rem;
+  padding: 0.75rem 1.5rem;
   outline: none;
   border: none;
-  border-radius: 4px;
+  border-radius: 8px;
   background-color: #09798C;
-
-  &:hover {
-    cursor: pointer;
-  }
 `;
