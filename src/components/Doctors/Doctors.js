f8ede4fae9464796f22970df0f999884f15ce40b
rediet <redietkfle@gmail.com>
2025-03-05 17:56:44 +0300

    Optimize textual content

--- src/components/Doctors/Doctors.js
+++ src/components/Doctors/Doctors.js
@@ -26,10 +26,9 @@ const Doctors = () => {
         <MedicalTeamHeader>
           <SectionTitle>Building Stronger Minds, One Patient at a Time</SectionTitle>
           <SectionDescription>
-            We are proud to have assembled a team of exceptional mental health professionals who bring a wealth of expertise and experience to our clinic. 
-            Our psychiatrists have trained at some of the most prestigious medical institutions across the world and are recognized leaders in their respective fields. 
-            With a deep understanding of the complexities of the human mind, our clinicians employ the latest evidence-based treatments to help our patients navigate their mental
-            health journeys with compassion and care. 
+            We are proud to have assembled a team of exceptional professionals with a wealth of expertise. Our psychiatrists have trained at some of top-ranking medical institutions 
+            across the world and are recognized leaders in their respective fields. With a deep understanding of the complexities of the human mind, our clinicians employ the latest 
+            evidence-based treatments to help our patients navigate their mental health journeys with compassion and care. 
             <Image src={greyLeaf} width={500} height={500} alt="Decorative Leaf Image"></Image> 
           </SectionDescription>
         </MedicalTeamHeader>
